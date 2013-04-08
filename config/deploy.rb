3107d559a493e41d285a89f0a9291a4e5ef20ccb
Pasi Lindholm <pasilindholm@Pasis-MacBook-Air-2.local>
2013-04-08 19:39:04 +0300

    bundler

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config/deploy.rb
 # $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) #
 # require 'rvm/capistrano'  # Add RVM integration
-# require 'bundler/capistrano'  # Add Bundler integration
+require 'bundler/capistrano'  # Add Bundler integration
 # load 'deploy/assets'  
 # only for rails 3.1 apps, this makes sure our assets are precompiled.
 
@@ -9,8 +9,6 @@ set :application, "simple_cms"
 role :web, "54.228.151.235"  # Your HTTP server, Apache/etc
 role :app, "54.228.151.235"  # This may be the same as your `Web` server
 role :db,  "54.228.151.235", :primary => true  # This is where Rails migrations will run
-
-after "deploy", "deploy:bundle_gems"
 
 set :scm, :git
 set :repository, "git@github.com:pasilindholm/cmstest.git"
